99ec281b084ce27eb84892aafbc34a089c0ef823
dalecook <dale@dalecook.com>
2018-01-07 22:44:00 -0800

    Move declares around a little

--- server/server.js
+++ server/server.js
@@ -1,15 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
 var app = express();
 const port = process.env.PORT || 3000;
-
-const {ObjectID} = require('mongodb');
-
-var {Todo} = require('./models/Todo');
-var {User} = require('./models/User');
 
 app.use(bodyParser.json());
 
